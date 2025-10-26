d520946c6af439b6bb56a0f55d89e87e6f998e94
GANAPATI <ganapathigouda379@gmail.com>
2025-10-26 22:28:09 +0530

    vercel-push2

--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -5,7 +5,7 @@ import { Button } from '../components/ui/button';
 import { Card, CardContent } from '../components/ui/card';
 import { Badge } from '../components/ui/badge';
 import { useApp } from '../context/AppContext';
-import { toast } from 'sonner@2.0.3';
+import { toast } from 'sonner';
 import { ImageWithFallback } from '../components/figma/ImageWithFallback';
 import { Link } from 'react-router-dom';
 
